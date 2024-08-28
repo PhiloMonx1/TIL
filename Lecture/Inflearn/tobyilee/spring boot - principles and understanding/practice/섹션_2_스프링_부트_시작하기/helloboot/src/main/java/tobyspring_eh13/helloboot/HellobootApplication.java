ed6bdb09eeca22d4a59fb0bc67269d6ca3173f09
PhiloMonx1 <philomonx1@gmail.com>
2024-08-29 00:26:36 +0900

    🌱📗Docs : [강의 : 토비의 스프링부트] 섹션 III 12_서블릿 컨테이너 띄우기
    *feat : `HellobootApplication.java` 메인 메서드 Tomcat 서버 실행 코드 추가

--- Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/helloboot/HellobootApplication.java
+++ Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/helloboot/HellobootApplication.java
@@ -1,4 +1,13 @@
 package tobyspring_eh13.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+    }
 }
