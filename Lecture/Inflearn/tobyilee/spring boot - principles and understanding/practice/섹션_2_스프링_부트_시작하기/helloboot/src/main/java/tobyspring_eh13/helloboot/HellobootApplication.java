4e58cfb0025e7a7f75eec168a5de968ad7c2e23a
PhiloMonx1 <philomonx1@gmail.com>
2024-09-03 02:20:23 +0900

    🌱📗Docs : [강의 : 토비의 스프링부트] 섹션 IV 18_스프링 컨테이너 사용
    *refactor : `HellobootApplication.java` HelloController Bean 등록해서 사용하도록 개선 (스프링 컨테이너 사용)
    *refactor : `HellobootApplication.java` 프론트컨트롤러 불필요한 코드 생략하여 가독성 개선

--- Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/helloboot/HellobootApplication.java
+++ Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/helloboot/HellobootApplication.java
@@ -3,7 +3,7 @@ package tobyspring_eh13.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,25 +15,24 @@ import java.io.IOException;
 
 public class HellobootApplication {
     public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
-
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
                     //인증, 보안, 다국어 등 공통 기능
-
                     if(request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())){
                         String name = request.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String returnHello = helloController.hello(name);
 
-                        response.setStatus(HttpStatus.OK.value());
-                        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         response.getWriter().println(returnHello);
-                    } else if (request.getRequestURI().equals("/user")) {
-                        //유저 관련 기능
                     } else {
                         response.setStatus(HttpStatus.NOT_FOUND.value());
                     }
