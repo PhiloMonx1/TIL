994cb4164696b20466229247923871f2dedddc6d
PhiloMonx1 <philomonx1@gmail.com>
2024-09-07 01:58:54 +0900

    🌱📗Docs : [강의 : 토비의 스프링부트] 섹션 IV 22_DispatcherServlet으로 전환

--- Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/helloboot/HellobootApplication.java
+++ Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/helloboot/HellobootApplication.java
@@ -3,43 +3,21 @@ package tobyspring_eh13.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class HellobootApplication {
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontController", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-                    //인증, 보안, 다국어 등 공통 기능
-                    if(request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())){
-                        String name = request.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String returnHello = helloController.hello(name);
-
-                        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        response.getWriter().println(returnHello);
-                    } else {
-                        response.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-
-
-                }
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext) {
             }).addMapping("/*");
         });
         webServer.start();
