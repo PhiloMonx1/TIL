217381c665597f7b0c043b4c8782462ae97c76ba
PhiloMonx1 <philomonx1@gmail.com>
2024-09-14 13:33:16 +0900

    🌱📗Docs : [강의 : 토비의 스프링부트] 섹션 V 29_테스트 코드를 이용한 테스트

---
+++ Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/test/java/tobyspring_eh13/helloboot/HelloApiTest.java
@@ -0,0 +1,28 @@
+package tobyspring_eh13.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> response =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code : 200
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header content-type: text/plain
+        assertThat(response.getHeaders().getFirst("Content-Type")).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body: Hello Spring
+        assertThat(response.getBody()).isEqualTo("Hello Spring");
+        // body: Hello Spring
+    }
+}
