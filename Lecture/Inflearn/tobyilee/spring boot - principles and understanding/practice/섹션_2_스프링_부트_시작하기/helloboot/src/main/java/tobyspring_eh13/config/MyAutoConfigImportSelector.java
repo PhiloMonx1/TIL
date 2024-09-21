1ff6d93504d6c29945ba6808e0fd171fc97aab3e
PhiloMonx1 <philomonx1@gmail.com>
2024-09-21 14:52:45 +0900

    🌱📗Docs : [강의 : 토비의 스프링부트] 섹션 VI 37_자동 구성 정보 파일 분리

--- Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/config/MyAutoConfigImportSelector.java
+++ Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,21 @@
 package tobyspring_eh13.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-          "tobyspring_eh13.config.autoconfig.DispatcherServletConfig",
-          "tobyspring_eh13.config.autoconfig.TomcatWebServerConfig",
-        };
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
