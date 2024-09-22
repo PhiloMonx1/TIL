fb58028be4ffd1bc78a36f009986dd63bc4ed3c0
PhiloMonx1 <philomonx1@gmail.com>
2024-09-22 19:53:40 +0900

    🌱📗Docs : [강의 : 토비의 스프링부트] 섹션 VI 38_자동 구성 애노테이션 적용

--- Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/config/autoconfig/DispatcherServletConfig.java
+++ Lecture/Inflearn/tobyilee/spring boot - principles and understanding/practice/섹션_2_스프링_부트_시작하기/helloboot/src/main/java/tobyspring_eh13/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package tobyspring_eh13.config.autoconfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring_eh13.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
